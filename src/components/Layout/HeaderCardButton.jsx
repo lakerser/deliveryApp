f1b5b0707d8ee45fc26b7b8e71b929eaece3f6db
Taras Doloshytskyi <doloshytskyi@afreshed.at>
2022-06-03 16:47:45 +0300

    lesson149 animated button

--- src/components/Layout/HeaderCardButton.jsx
+++ src/components/Layout/HeaderCardButton.jsx
@@ -1,15 +1,29 @@
+/* eslint-disable consistent-return */
 /* eslint-disable max-len */
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import CartIcon from '../Cart/CartIcon';
 import classes from './HeaderCardButton.module.css';
 import cartContext from '../../store/cart-context';
 
 function Headercardbutton({ onShow }) {
+  const [btnHiglite, setButtonHighlighted] = useState(false);
   const context = useContext(cartContext);
-
   const numbverOfCartItems = context.items.reduce((currentNumber, item) => currentNumber + item.amount, 0);
+  const butnClasses = `${classes.button} ${btnHiglite ? classes.bump : ''}`;
+  useEffect(() => {
+    if (context.items.length === 0) {
+      return;
+    }
+    setButtonHighlighted(true);
+    const timer = setTimeout(() => {
+      setButtonHighlighted(false);
+    }, 300);
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [context.items]);
   return (
-    <button onClick={onShow} className={classes.button} type="button">
+    <button onClick={onShow} className={butnClasses} type="button">
       <span classes={classes.icon}>
         <CartIcon />
       </span>
